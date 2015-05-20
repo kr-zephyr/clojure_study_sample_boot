eea5370cb033d150cbe8c45b9f2d98cd02065830
SeungChul Lee <scin21c@gmail.com>
2015-05-20 10:25:54 +0900

    포트 설정 변경 가능하도록 설정 추가

---
+++ src/main/java/com/sz21c/samplebootweb/config/CustomizationBean.java
@@ -0,0 +1,15 @@
+package com.sz21c.samplebootweb.config;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomizationBean implements EmbeddedServletContainerCustomizer {
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.setPort(8080);
+    }
+
+}
